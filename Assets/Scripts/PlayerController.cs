656b257f466149c483c271bea29829f4771980e8
Stry <49200515+LegerTristan@users.noreply.github.com>
2021-01-31 22:47:43 +0100

    Adding project to git

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 4f, rotationSpeed = 100f;
+    private float currentSpeed;
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = speed * 2;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+        transform.Rotate(Vector3.up * rotationSpeed * Time.fixedDeltaTime * Input.GetAxis("Horizontal"));
+        transform.Translate(Vector3.right * currentSpeed * Time.fixedDeltaTime * Input.GetAxis("Vertical"));
+    }
+}
