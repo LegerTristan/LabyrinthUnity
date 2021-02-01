c9e8844e215f917ee88c0d8f15093a0ecef14061
Stry <49200515+LegerTristan@users.noreply.github.com>
2021-02-01 23:20:03 +0100

    Adding some functionnalities

---
+++ Assets/Scripts/DoorManagement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DoorManagement : MonoBehaviour
+{
+    public bool CanOpen = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && CanOpen)
+        {
+            GetComponent<Animator>().enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/KeyBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBehavior : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            GameObject.Find("DoorTrigger").GetComponent<DoorManagement>().CanOpen = true;
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
